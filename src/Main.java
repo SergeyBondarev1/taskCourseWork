2391c0c8b069f20ae60e67afffb686051556b51e
Сергей <bondarevser.bondarev@yandex.ru>
2023-06-09 23:26:51 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+
+        String arr[] = new String[]{"aa", "bb", "cc", "aa", "bb", "cc"};
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == null) continue;
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i].equals(arr[j])){
+
+                    arr[j] = null;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
